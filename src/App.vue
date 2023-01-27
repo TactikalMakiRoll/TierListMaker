44c0ad3c9e14a211753e4cefaa86fa0968ccf3a7
TactikalMakiRoll <sagridin@gmail.com>
2023-01-27 22:51:09 +0200

    making a modal

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,52 @@
 <template>
-  <div class="text-white px-14 py-8">
+  <div class="text-white px-4 py-8">
     <div class="inline-block focus-within::border-0 focus-within::outline-dashed 
     focus-within:outline-dashed break-words">
-    <span class="inline-block break-words px-1 py-2 border-0 bg-transparent 
-    focus: text-5xl outline-none placeholder:text-white resize-none
-    min-h-[1rem] leading-normal max-w-[35rem]" max=35 name="Tier Title" 
-    placeholder="Create your tier list" contenteditable
-    @keypress="limitHeaderLength($event)" @keyup="fillEmptyHeader($event)">Create your tier list</span></div>
-    <div class="mt-4 space-y-6">
-      <div class="py-5 border-solid border-8 rounded-full border-red-700">
-        <span class="px-8 border-solid border-r-red-700 border-r-8  text-7xl text-red-700">S</span>
+      <span class="inline-block break-words px-1 py-2 border-0 bg-transparent 
+      focus: text-4xl outline-none placeholder:text-white resize-none
+      min-h-[1rem] leading-normal max-w-[90vw]" max=35 name="Tier Title" 
+      placeholder="Create your tier list" contenteditable
+      @keypress="limitHeaderLength($event)" @keyup="fillEmptyHeader($event)">
+      Create your tier list</span>
+    </div>
+    <div class="mt-4 space-y-4">
+      <div class="py-5 border-solid border-4 rounded-full border-red-700">
+        <span class="px-4 border-solid border-r-red-700 border-r-4 text-3xl text-red-700">S</span>
       </div>
-      <div class="py-5 border-solid border-8 rounded-full border-sky-400">
-        <span class="px-8 border-solid border-r-sky-400 border-r-8  text-7xl text-sky-400 ">A</span>
+      <div class="py-5 border-solid border-4 rounded-full border-sky-400">
+        <span class="px-4 border-solid border-r-sky-400 border-r-4 text-3xl text-sky-400 ">A</span>
       </div>
-      <div class="py-5 border-solid border-8 rounded-full border-amber-300">
-        <span class="px-8 border-solid border-r-amber-300 border-r-8  text-7xl text-amber-300">B</span>
+      <div class="py-5 border-solid border-4 rounded-full border-amber-300">
+        <span class="px-4 border-solid border-r-amber-300 border-r-4 text-3xl text-amber-300">B</span>
       </div>
-      <div class="py-5 border-solid border-8 rounded-full border-green-300">
-        <span class="px-8 border-solid border-r-green-300 border-r-8  text-7xl text-green-300">C</span>
+      <div class="py-5 border-solid border-4 rounded-full border-green-300">
+        <span class="px-4 border-solid border-r-green-300 border-r-4 text-3xl text-green-300">C</span>
       </div>
-      <div class="py-5 border-solid border-8 rounded-full border-sky-400">
-        <span class="px-8 border-solid border-r-sky-400 border-r-8  text-7xl text-sky-400">D</span>
+      <div class="py-5 border-solid border-4 rounded-full border-sky-400">
+        <span class="px-4 border-solid border-r-sky-400 border-r-4 text-3xl text-sky-400">D</span>
       </div>
-      <div class="py-5 border-solid border-8 rounded-full border-purple-500">
-        <span class="px-8 border-solid border-r-purple-500 border-r-8  text-7xl text-purple-500">E</span>
+      <div class="py-5 border-solid border-4 rounded-full border-purple-500">
+        <span class="px-4 border-solid border-r-purple-500 border-r-4 text-3xl text-purple-500">E</span>
       </div>
-      <div class="py-5 border-solid border-8 rounded-full border-pink-500">
-        <span class="px-8 border-solid border-r-pink-500 border-r-8  text-7xl text-pink-500">F</span>
+      <div class="py-5 border-solid border-4 rounded-full border-pink-500">
+        <span class="px-4 border-solid border-r-pink-500 border-r-4 text-3xl text-pink-500">F</span>
       </div>
     </div>
+    <ItemForm ref="itemModal"></ItemForm>
   </div>
 </template>
 
 <script setup>
+import {ref} from 'vue';
 import TierComponent from './components/TierComponent.vue'
+import ItemForm from './components/ItemForm.vue';
+
+const itemModal = ref(null);
+
+function openModal(){
+  console.log("fjsdklafj");
+  itemModal.value.openModal();
+}
 
 function limitHeaderLength(e){
   if(e.target.innerHTML.length >= e.target.getAttribute("max")){
@@ -45,10 +57,7 @@ function limitHeaderLength(e){
 function fillEmptyHeader(e){
   if(e.target.textContent.length===0){
     e.target.textContent = "Input title";
-    e.target.select();
   }
-  console.log(e.target.textContent.length);
-  console.log(e.target.textContent.length===0);
 }
 </script>
 
---
+++ src/components/ItemForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <button @click="openModal()" class="active:scale-95 active:shadow-xl shadow-white block mx-auto bg-violet-700 rounded-full px-4 py-2 text-2xl mt-10" >Add an item</button>
+        <dialog class="border-white border-2 text-white bg-violet-800 rounded-xl backdrop:bg-[rgba(0,0,0,0.8)]" ref="modal">
+            <form class="space-y-4" method="dialog">
+                <div class="grid gap-y-1">
+                    <label for="name">Item name:</label>
+                    <input minlength="3" maxlength="25" class="focus:outline-white px-2 bg-gray-900 rounded-xl" type="text" name="name" id="name">
+                </div>
+                <div class="grid gap-y-1">
+                    <label for="picture">Choose a profile picture:</label>
+                    <input type="file"
+                        id="picture" name="picture"
+                        class="px-2 py-2 bg-gray-900 "
+                        accept="image/png, image/jpeg">
+                </div>
+                <button class="block px-4 py-2 bg-gray-900 rounded-full mx-auto active:scale-95">Create</button>
+            </form>
+        </dialog>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const modal = ref(null);
+
+function openModal(){
+    modal.value.showModal();
+}
+
+</script>
+
+<style>
+
+</style>
